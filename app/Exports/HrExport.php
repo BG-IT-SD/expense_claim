acc4d6fa945455808c785ab7a7081d4b4b47817b
kamolwanbuncha <kamolwan.b@bgiglass.com>
2025-07-02 11:20:23 +0700

    Edit HR Report

--- app/Exports/HrExport.php
+++ app/Exports/HrExport.php
@@ -3,8 +3,12 @@ namespace App\Exports;
 
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use Maatwebsite\Excel\Concerns\WithDrawings;
 
-class HrExport implements FromView
+class HrExport implements FromView, WithStyles
 {
     protected $expenses;
 
@@ -16,7 +20,24 @@ class HrExport implements FromView
     public function view(): View
     {
         return view('exports.hr-list', [
-            'expenses' => $this->expenses
+            'expenses' => $this->expenses,
         ]);
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]], // แถวหัวตารางตัวหนา
+            'A1:Z1' => [ // จัด style ช่วงหัวตาราง
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'startColor' => ['rgb' => 'D9D9D9'],
+                ],
+                'alignment' => [
+                    'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
+                ],
+                'font' => ['size' => 12],
+            ],
+        ];
     }
 }
